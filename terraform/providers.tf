77bf98b36b0c29e7c76da3827b0bbccd8a215d45
Bernardo Augusto Garcia <botibagl@gmail.com>
2021-10-28 13:42:41 +0200

    Passing variables

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,8 +13,10 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {}
-  username = var.username
-  password = var.password
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 
 }
 
